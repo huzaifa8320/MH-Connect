15e369ca9e20500be96aed941ec7eec4434c3135
huzaifa8320 <muhammadhuzaifa8320@gmailcom>
2024-10-06 21:54:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='h-screen gap-8 flex flex-col items-center justify-center bg-black text-white'>
+      <h1 className='font-bold text-4xl'>MH-Connect Comming Soon 😉</h1>
+      <h1 className='font-bold text-4xl'>MH-Connect Comming Soon 😉</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
